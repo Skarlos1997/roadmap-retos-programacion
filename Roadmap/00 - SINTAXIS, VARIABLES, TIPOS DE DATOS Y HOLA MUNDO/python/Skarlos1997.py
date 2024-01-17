04d9a4059827aea837dcadcb353f3d2ab4bccc50
Luchosky <69882450+Skarlos1997@users.noreply.github.com>
2024-01-17 09:54:59 -0500

    Create Skarlos1997.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Skarlos1997.py
@@ -0,0 +1,14 @@
+# Retos de programación Roadmap 2024 - Reto #0, Python - Skarlos1997
+# Pagina de python: https://www.python.org/
+# Comentario de una sola linea
+"""
+    Comentario de multi-lineas
+"""
+
+var1 = 1
+entero = 2
+flotante = 3.14
+cadena = "Hola mundo"
+booleano = True
+
+print('!Hola, python!')
